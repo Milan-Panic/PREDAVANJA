0d81637d2bd4736e01caaf47e8bf8f8c227dbca2
milanpanic <milanpanic88@gmail.com>
2020-02-03 12:52:22 +0100

    Pred

---
+++ Predavanje 2.03/prvi.js
@@ -0,0 +1,85 @@
+// var x = 10;
+// switch (x) {
+//     case 0:
+//         console.log('Januar ima 31');
+
+//         break;
+//     case 1:
+//         console.log('Februar ima 28/29');
+
+//         break;
+//     case 2:
+//         console.log('Mart ima 31');
+
+//         break;
+//     case 3:
+//         console.log('April ima 30');
+
+//         break;
+//     case 4:
+//         console.log('Maj ima 31');
+
+//         break;
+//     case 5:
+//         console.log('Jun ima 30');
+
+//         break;
+//     case 6:
+//         console.log('Jul ima 31');
+
+//         break;
+//     case 7:
+//         console.log('Decembar ima 31');
+
+//         break;
+//     case 8:
+//         console.log('Septembar ima 30');
+
+//         break;
+//     case 9:
+//         console.log('Oktobar ima 31');
+
+//         break;
+//     case 10:
+//         console.log('Novembar ima 30');
+
+//         break;
+
+//     case 11:
+//         console.log('Decembar ima 31');
+
+//         break;
+
+//     default:
+//         console.log('Niste uneli ispravan mesec');
+
+//         break;
+// }
+// let year = 2021;
+// if ((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0) {
+//     console.log('Godina je prestupna');
+
+// } else {
+//     console.log('Godina nije prestupna');
+
+// }
+
+let broj = 123;
+// console.log(broj % 10)
+// broj = Math.floor(broj / 10);
+// console.log(broj % 10)
+// broj = Math.floor(broj / 10);
+// console.log(broj % 10)
+
+//  5 +'' OVAKO BROJ PRETVARAMO U STRING
+//let jed = b % 10;
+//odrediti da li je trocifren broj amtsrongov broj 153 = 1^3 + 5^3 + 3^3
+// let br = 153;
+// let br3 = br % 10;
+// let br2 = Math.floor(br / 10) % 10;
+// let br1 = Math.floor(br / 100) % 10;
+// console.log(br3, br2, br1);
+// if (br == (Math.pow(br3, 3) + Math.pow(br2, 3) + Math.pow(br1, 3))) {
+//     console.log('Jeste');
+
+// } else console.log('Nije');
